5fed0d92f9e5e2123f99581c36d83530bd5ddc60
wanwenlong <wanwenlong@xinanli.com>
2024-10-31 18:04:48 +0800

    feat(webflux): 添加 webflux项目基础结构和配置
    - 创建项目目录结构和基本文件
    - 添加 Maven 配置文件 pom.xml
    - 编写简单的 Webflux 应用程序入口类
    - 添加单元测试类
    - 配置 Git 属性和忽略文件

---
+++ webflux/src/main/java/com/platform/ahj/webflux/WebfluxApplication.java
@@ -0,0 +1,43 @@
+package com.platform.ahj.webflux;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.core.io.buffer.DataBufferFactory;
+import org.springframework.http.server.reactive.HttpHandler;
+import org.springframework.http.server.reactive.ReactorHttpHandlerAdapter;
+import reactor.core.publisher.Mono;
+import reactor.netty.DisposableServer;
+import reactor.netty.http.server.HttpServer;
+
+import java.nio.charset.StandardCharsets;
+
+// @SpringBootApplication
+@Slf4j
+public class WebfluxApplication {
+
+    public static void main(String[] args) {
+
+        HttpHandler httpHandler = (request, response) -> {
+            log.info("请求路径：{}", request.getURI());
+            //编写请求处理的业务,给浏览器写一个内容 URL + "Hello~!"
+            //            response.getHeaders(); //获取响应头
+            //            response.getCookies(); //获取Cookie
+            //            response.getStatusCode(); //获取响应状态码；
+            //            response.bufferFactory(); //buffer工厂
+            //            response.writeWith() //把xxx写出去
+            //            response.setComplete(); //响应结束
+            DataBufferFactory dataBufferFactory = response.bufferFactory();
+            DataBuffer wrap = dataBufferFactory.wrap(new String("hello!").getBytes(StandardCharsets.UTF_8));
+            return response.writeWith(Mono.just(wrap));
+        };
+        ReactorHttpHandlerAdapter adapter = new ReactorHttpHandlerAdapter(httpHandler);
+        DisposableServer localhost = HttpServer.create()
+                                               .host("localhost")
+                                               .port(8080)
+                                               .handle(adapter)
+                                               .bindNow();
+        localhost.onDispose().block();
+        // SpringApplication.run(WebfluxApplication.class, args);
+    }
+
+}
