710ea854dee56f2a484eaebb736c0bbc828c52ec
wanwenlong <wanwenlong@xinanli.com>
2024-11-05 16:35:59 +0800

    feat(r2dbc-mysql): 添加一对一书籍作者查询功能
    - 新增 oneToMany 测试方法，实现作者和书籍的一对多查询- 添加 TBookAuthor 实体类，用于映射书籍和作者的一对一关系
    - 优化 testJoin 方法，增加findAll查询并调整输出日志
    - 在测试类中添加 DatabaseClient 注入，用于执行复杂查询

--- r2dbc-mysql/src/test/java/com/platform/ahj/r2dbcmysql/R2dbcMysqlApplicationTests.java
+++ r2dbc-mysql/src/test/java/com/platform/ahj/r2dbcmysql/R2dbcMysqlApplicationTests.java
@@ -1,6 +1,8 @@
 package com.platform.ahj.r2dbcmysql;
 
 import com.platform.ahj.r2dbcmysql.controller.UserController;
+import com.platform.ahj.r2dbcmysql.entity.TAuthor;
+import com.platform.ahj.r2dbcmysql.entity.TBook;
 import com.platform.ahj.r2dbcmysql.repositories.BookRepository;
 import com.platform.ahj.r2dbcmysql.repositories.UserRepository;
 import io.r2dbc.spi.Connection;
@@ -8,12 +10,18 @@ import io.r2dbc.spi.ConnectionFactories;
 import io.r2dbc.spi.ConnectionFactory;
 import io.r2dbc.spi.ConnectionFactoryOptions;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.r2dbc.core.DatabaseClient;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import static io.r2dbc.spi.ConnectionFactoryOptions.*;
 
@@ -30,6 +38,14 @@ class R2dbcMysqlApplicationTests {
 
     @Autowired
     private BookRepository bookRepository;
+
+    @Autowired
+    private DatabaseClient databaseClient;
+
+    @AfterAll
+    static void close() throws InterruptedException {
+        Thread.sleep(10000);
+    }
 
     @BeforeAll
     static void init() {
@@ -68,6 +84,8 @@ class R2dbcMysqlApplicationTests {
 
     @Test
     void testJoin() throws InterruptedException {
+        bookRepository.findAll()
+                      .subscribe(x -> log.info("findAll:{}", x));
         bookRepository.hahaBook(1l)
                       .subscribe(x -> log.info("hahaBook:{}", x));
         Thread.sleep(10000);
@@ -84,6 +102,71 @@ class R2dbcMysqlApplicationTests {
                       .subscribe(v -> {
                           System.out.println(v);
                       });
+        Thread.sleep(10000);
+    }
+
+    @Test
+    void oneToMany() throws InterruptedException {
+        //        databaseClient.sql("select a.id aid,a.name,b.* from t_author a  " +
+        //                "left join t_book b on a.id = b.author_id " +
+        //                "order by a.id")
+        //                .fetch()
+        //                .all(row -> {
+        //
+        //                })
+
+
+        // 1~6
+        // 1：false 2：false 3:false 4: true 8:true 5:false 6:false 7:false 8:true 9:false 10:false
+        // [1,2,3]
+        // [4,8]
+        // [5,6,7]
+        // [8]
+        // [9,10]
+        // bufferUntilChanged：
+        // 如果下一个判定值比起上一个发生了变化就开一个新buffer保存，如果没有变化就保存到原buffer中
+
+        //        Flux.just(1,2,3,4,8,5,6,7,8,9,10)
+        //                .bufferUntilChanged(integer -> integer%4==0 )
+        //                .subscribe(list-> System.out.println("list = " + list));
+        ; // 自带分组
+
+
+        Flux<TAuthor> flux = databaseClient.sql("""
+                                                select a.id aid,a.name,b.* from t_author a  
+                                                       left join t_book b on a.id = b.author_id 
+                                                       order by a.id
+                                                """)
+                                           .fetch()
+                                           .all()
+                                           .bufferUntilChanged(rowMap -> Long.parseLong(rowMap.get("aid")
+                                                                                              .toString()))
+                                           .map(list -> {
+                                               TAuthor tAuthor = new TAuthor();
+                                               Map<String, Object> map = list.get(0);
+                                               tAuthor.setId(Long.parseLong(map.get("aid")
+                                                                               .toString()));
+                                               tAuthor.setName(map.get("name")
+                                                                  .toString());
+                                               // 查到的所有图书
+                                               List<TBook> tBooks = list.stream()
+                                                                        .map(ele -> {
+                                                                            TBook tBook = new TBook();
+                                                                            tBook.setId(Long.parseLong(ele.get("id")
+                                                                                                          .toString()));
+                                                                            tBook.setAuthorId(Long.parseLong(
+                                                                                    ele.get("author_id")
+                                                                                       .toString()));
+                                                                            tBook.setTitle(ele.get("title")
+                                                                                              .toString());
+                                                                            return tBook;
+                                                                        })
+                                                                        .collect(Collectors.toList());
+
+                                               tAuthor.setBooks(tBooks);
+                                               return tAuthor;
+                                           });// Long 数字缓存 -127 - 127；// 对象比较需要自己写好equals方法
+        flux.subscribe(tAuthor -> log.info("tAuthor = {}", tAuthor));
         Thread.sleep(10000);
     }
 
